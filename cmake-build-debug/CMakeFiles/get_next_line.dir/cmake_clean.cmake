14355682cdef0d8bf5d88dc784527e7e2217a370
Ignacio Ortego De La Mata <iortego-@c2r14s3.42madrid.com>
2022-09-06 11:41:33 +0200

    clion_proyect

---
+++ cmake-build-debug/CMakeFiles/get_next_line.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/get_next_line.dir/main.c.o"
+  "get_next_line"
+  "get_next_line.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/get_next_line.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/get_next_line.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for get_next_line.
+# This may be replaced when dependencies are built.
